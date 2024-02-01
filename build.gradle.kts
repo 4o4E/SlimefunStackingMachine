4f5a141c6a949f0551b13b4aef1edbd0d96356c9
4o4E <869951226@qq.com>
2024-02-01 17:29:16 +0800

    update: 1.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "top.e404"
-version = "1.3.1"
+version = "1.3.2"
 val epluginVersion = "1.2.0"
 
 fun kotlinx(id: String, version: String) = "org.jetbrains.kotlinx:kotlinx-$id:$version"
